7427e77aba658d7c15629ce3e9a4733850202ace
Gabriel Marques Magalhães <gabriel.magalhaes.dev@gmail.com>
2025-11-26 16:32:13 -0300

    correct general points

--- fastify/zod+biome+vitest/src/modules/hello/hello.schemas.ts
+++
@@ -1,5 +0,0 @@
-import { z } from "zod";
-
-export const helloResponseSchema = z.object({
-	message: z.string()
-});
--- fastify/zod+biome+vitest/src/modules/hello/index.ts
+++
@@ -1,7 +0,0 @@
-import { helloRoutes } from "@modules/hello/hello.routes";
-import type { FastifyPluginAsync } from "fastify";
-import fp from "fastify-plugin";
-
-export const helloModule: FastifyPluginAsync = fp(async (app) => {
-	app.register(helloRoutes);
-});
